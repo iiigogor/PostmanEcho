33440764e7248a638c40928120307c4676bb7d50
unknown <iiigogor@gmail.com>
2022-02-21 21:52:16 +0300

    addedd commit

---
+++ src/test/java/ru/netology/postmanEcho/postmanEchoTest.java
@@ -0,0 +1,28 @@
+package ru.netology.postmanEcho;
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class postmanEchoTest {
+    @Test
+    void shouldPostmanEchoAccounts() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data")
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+}
+
