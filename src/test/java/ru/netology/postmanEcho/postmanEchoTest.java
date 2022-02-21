e82341e622cb9b4fc74c97fa7c4c775961a49d24
iiigogor <92747953+iiigogor@users.noreply.github.com>
2022-02-21 22:18:35 +0300

    Update postmanEchoTest.java
    do Success

--- src/test/java/ru/netology/postmanEcho/postmanEchoTest.java
+++ src/test/java/ru/netology/postmanEcho/postmanEchoTest.java
@@ -21,7 +21,7 @@ public class postmanEchoTest {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
